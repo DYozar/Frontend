68ae45ce823a8c020decc6421c9531fd223767da
DYozar <donyozar@hotmail.com>
2023-11-20 22:08:35 +0100

    all

--- src/component/Sections/Categorie'sSection/index.js
+++ src/component/Sections/Categorie'sSection/index.js
@@ -27,7 +27,7 @@ const DynamicSection = ({ categorySlug, categoryTitle, categoryPosts }) => {
       </h2>
 
       <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8 ">
-        <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:grid-cols-6 xl:gap-x-8">
+        <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-6 xl:grid-cols-6 xl:gap-x-8">
           {categoryPosts.map((post, index) => {
             let content;
             
