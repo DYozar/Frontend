6fcc6d2fec210ddeda5a9a1fdf7617214109c93b
DYozar <donyozar@hotmail.com>
2023-11-28 08:50:26 +0100

    all

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,13 +1,13 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: 'http://localhost:3000/',
+    siteUrl: 'https://insightmedium.blog/',
     generateRobotsTxt: true,
     changefreq: 'daily',
     priority: 0.7,
     exclude: ['/sitemap.xml'], // <= exclude here
     robotsTxtOptions: {
       additionalSitemaps: [
-        'http://localhost:3000/sitemap.xml', // <==== Add here
+        'https://insightmedium.blog/sitemap.xml', // <==== Add here
       ],
     },
   }
