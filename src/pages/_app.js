a617294ead6ad551a32a7efaecdc212ff056dde7
DYozar <donyozar@hotmail.com>
2023-11-30 22:30:07 +0100

    all

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -30,7 +30,6 @@ export default function App({ Component, pageProps }) {
       <Head>
         <meta name="p:domain_verify" content="71defa7e69311be4efaec119b7778f92"/>
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"/>
-        <meta property="og:image" content={SiteLogo} />
         <meta name="description" content="Unlock the world of financial wisdom and business brilliance. Dive into market mastery, global economic stories, and insider insights. Stay empowered with our daily dose of savvy tips and curated stories. Explore the intersection of knowledge and success with our comprehensive media platform."/>
         <title> Navigate Success: Your Gateway to Market Mastery, Business Brilliance, and Global Economic Insights</title>
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6994792846221170"crossorigin="anonymous"></script>
