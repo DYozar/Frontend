db21cd9f1e0a41c2109387a90715c4572006cc31
DYozar <donyozar@hotmail.com>
2023-11-30 20:13:51 +0100

    all

--- src/pages/Article/[slug].js
+++ src/pages/Article/[slug].js
@@ -28,12 +28,12 @@ useEffect(() => {
       
       <Head>
         <title>{posts[0].Title}</title>
-        <meta property="og:title" content={posts.Title} />
-        <meta property="og:description" content={posts[0].Content} />
+        <meta name="title" content={posts[0].Title} />
+        <meta property="article:publisher" content="InsightMedium"></meta>
+        <meta name="og:description" content={posts[0].Content} />
         {posts[0].Media && Array.isArray(posts[0].Media) && posts[0].Media.length > 0 && (
           posts[0].Media.map((media, index) => 
             {
-
             return <meta property="og:image" content={media.url} key={index} />}
           )
         )}
