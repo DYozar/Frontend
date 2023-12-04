edb7877ab5e08245fe7ec9d8d68c79ee12cb512c
DYozar <donyozar@hotmail.com>
2023-12-04 18:51:06 +0100

    all

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -41,7 +41,7 @@ export default function App({ Component, pageProps }) {
         <meta property="og:type" content="website" />
         <meta property="og:title" content=" Navigate Success: Your Gateway to Market Mastery, Business Brilliance, and Global Economic Insights" />
         <meta property="og:description" content="Unlock the world of financial wisdom and business brilliance. Dive into market mastery, global economic stories, and insider insights. Stay empowered with our daily dose of savvy tips and curated stories. Explore the intersection of knowledge and success with our comprehensive media platform." />
-        <meta property="og:image" content="/asset/sitelogo.png" />
+        <meta property="og:image" content="src/asset/sitelogo.png" />
         <meta property="og:image:secure_url" content={SiteLogo} />
         <meta property="og:image:type" content="image/png" />
         <meta property="og:image:width" content="500"/>
@@ -49,7 +49,7 @@ export default function App({ Component, pageProps }) {
         <meta property="og:type" content="article" />
         <meta http-equiv="refresh" content="30"></meta>
         <meta name="keywords" content="financial markets, business strategies, global economy, market mastery, business brilliance, financial tips"/>
-        <link rel="icon" type="image/x-icon" href="/public/favicon.ico"></link>
+        <link rel="icon" type="image/x-icon" href="public/favicon.ico"></link>
       </Head>
       <Layout>
         <Component {...pageProps} />
