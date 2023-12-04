a5a0b43ab225a661bb5250ea0f9e38ebb3c66d84
DYozar <donyozar@hotmail.com>
2023-12-04 18:40:16 +0100

    all

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -32,15 +32,11 @@ export default function App({ Component, pageProps }) {
       <Head>
         <meta name="p:domain_verify" content="71defa7e69311be4efaec119b7778f92"/>
         <meta name="p:domain_verify" content="71defa7e69311be4efaec119b7778f92"/>
-        <meta property="og:image" content={SiteLogo}></meta>
-        <meta property="og:image:type" content="image/png" />
-        <meta property="og:image:width" content="500"/>
-        <meta property="og:image:height" content="500"/>
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"/>
         <meta name="description" content="Unlock the world of financial wisdom and business brilliance. Dive into market mastery, global economic stories, and insider insights. Stay empowered with our daily dose of savvy tips and curated stories. Explore the intersection of knowledge and success with our comprehensive media platform."/>
         <title> Navigate Success: Your Gateway to Market Mastery, Business Brilliance, and Global Economic Insights</title>
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6994792846221170"crossorigin="anonymous"></script>
-        
+        <Image src={SiteLogo} width={500} height={500} alt="Insight" />
       </Head>
       <Layout>
         <Component {...pageProps} />
