62818ebbbb441f2f599557ff3b1e1bb1b438d438
DYozar <donyozar@hotmail.com>
2023-12-24 15:34:55 +0100

    all

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -16,7 +16,7 @@ export default function App({ Component, pageProps }) {
       {/* Insert the Google Tag Manager code here */}
       {/* Google Tag (gtag.js) */}
       <Script async src="https://www.googletagmanager.com/gtag/js?id=G-L8H31NEQ94"/>
-      
+
       <Script
         dangerouslySetInnerHTML={{
           __html: `
@@ -28,15 +28,15 @@ export default function App({ Component, pageProps }) {
         }}
       /> 
       {/* End Google Tag Manager code */}
-      
+
       <Head>
         <meta name="p:domain_verify" content="71defa7e69311be4efaec119b7778f92"/>
         <meta name="p:domain_verify" content="71defa7e69311be4efaec119b7778f92"/>
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"/>
         <meta name="description" content="Unlock the world of financial wisdom and business brilliance. Dive into market mastery, global economic stories, and insider insights. Stay empowered with our daily dose of savvy tips and curated stories. Explore the intersection of knowledge and success with our comprehensive media platform."/>
         <title> Navigate Success: Your Gateway to Market Mastery, Business Brilliance, and Global Economic Insights</title>
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6994792846221170"crossorigin="anonymous"></script>
-        <meta property="og:type" content="website" />
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3404844893519470"
+     crossorigin="anonymous"></script>        <meta property="og:type" content="website" />
         <meta property="og:title" content=" Navigate Success: Your Gateway to Market Mastery, Business Brilliance, and Global Economic Insights" />
         <meta property="og:description" content="Unlock the world of financial wisdom and business brilliance. Dive into market mastery, global economic stories, and insider insights. Stay empowered with our daily dose of savvy tips and curated stories. Explore the intersection of knowledge and success with our comprehensive media platform." />
         <meta property="og:image" content="https://insightmedium.blog/_next/static/media/sitelogo.ae447203.png" />
@@ -47,6 +47,7 @@ export default function App({ Component, pageProps }) {
         <meta property="og:type" content="article" />
         <meta name="keywords" content="Financial Markets, Stock Trading, Investment Strategies, Market Trends, Business Insights, Economic Forecast, Financial Analysis, Stock Market News, Entrepreneurship, Startup Success, Market Mastery, Trading Tips, Global Economy, Economic Trends, Market Dynamics, Financial News, Business Brilliance, Financial Growth, Wealth Management, Investment Portfolio, Financial Planning, Market Research, Business Development, Investment Opportunities, Economic Outlook, Market Intelligence, Financial Strategies, Business Innovation, Market Leadership, Investment Insights, Economic Impact, Market Expansion, Financial Wellness, Business Success, Market Analysis, Trading Trends, Economic Growth, Financial Literacy, Startup Growth, Market Performance, Investment Risks, Business Excellence, Financial Advice, Wealth Building, Market Opportunities, Economic Stability, Financial Markets Today, Business Trends, Investment Mastery, Market Volatility, Financial Management, Startup Tips, Economic Strategies, Market Challenges, Business Innovation, Financial Freedom, Wealth Creation, Investment Planning, Market Intelligence, Economic Insights, Financial Success, Business Leadership, Market Trends Today, Trading Insights, Economic News, Financial Wellness, Startup Strategies, Market Growth, Investment Advice, Economic Impact, Business Opportunities, Financial Growth Tips, Wealth Strategies, Market Analysis, Business Planning, Investment Trends, Economic Outlook, Financial Strategies, Business Development, Market Leadership, Investment Insights, Economic Impact, Market Expansion, Financial Wellness, Business Success, Market Analysis, Trading Trends, Economic Growth, Financial Literacy, Startup Growth, Market Performance, Investment Risks, Business Excellence, Financial Advice, Wealth Building, Market Opportunities, Economic Stability, Financial Markets Today, Business Trends, Investment Mastery" />
         <link rel="icon" type="image/x-icon" href="public\favicon.ico"></link>
+        <meta name="google-adsense-account" content="ca-pub-3404844893519470"></meta>
       </Head>
       <Layout>
         <Component {...pageProps} />
